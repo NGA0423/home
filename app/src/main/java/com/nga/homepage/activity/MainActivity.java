8a5ad9b73a061c02152748ec3795d354d51114b4
huatec-1 <1834291158@qq.com>
2019-10-08 11:46:09 +0800

    添加文件夹

--- app/src/main/java/com/nga/homepage/activity/MainActivity.java
+++ app/src/main/java/com/nga/homepage/activity/MainActivity.java
@@ -1,10 +1,12 @@
-package com.nga.homepage;
+package com.nga.homepage.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+
+import com.nga.homepage.R;
 
 public class MainActivity extends AppCompatActivity {
 
